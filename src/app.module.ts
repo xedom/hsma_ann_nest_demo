f358017ce9b61a7851e7d12eebb0c4b002d1d7e6
Kaj Oskar Rusilowski <xedom08@gmail.com>
2023-11-04 12:27:02 +0100

    add mongoose (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot();
+console.log(process.env.MONGO_URI);
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot(), //environment variables
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

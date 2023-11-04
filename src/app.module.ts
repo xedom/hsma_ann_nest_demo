784d8f9aefafab9c08c3dc57f2622c852f8b4ca6
niklas <niklas.kerner@proton.me>
2023-11-04 17:19:22 +0100

    throttler and helmet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { ProductsController } from './products/products.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,6 +6,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
+import { ThrottlerModule } from "@nestjs/throttler";
 
 ConfigModule.forRoot();
 console.log(process.env.MONGO_URI);
@@ -13,10 +16,17 @@ console.log(process.env.MONGO_URI);
   imports: [
     UsersModule,
     AuthModule,
+    ProductsModule,
     ConfigModule.forRoot(), //environment variables
     MongooseModule.forRoot(process.env.MONGO_URI),
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
   ],
-  controllers: [AppController],
+  controllers: [
+    AppController
+  ],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
